62c03c3e0b8a1fd5960fed58941f182326a795fa
Garret Kanwischer <kanwischer.garret@gmail.com>
2023-04-17 17:19:04 -0500

    created the addTo function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,2 +1,11 @@
 console.log('***** Music Collection *****')
+
 let collection = [];
+
+function addToCollection( title, artist, yearPublished){ // adds a new album to the collection array
+    let newAlbum = { title, artist, yearPublished };
+    collection.push( newAlbum );
+    return newAlbum;
+} // end addToCollection
+
+
