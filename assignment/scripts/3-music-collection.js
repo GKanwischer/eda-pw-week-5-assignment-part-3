e0572d1418115776e94ee1cfbd4a1d42a9937214
Garret Kanwischer <kanwischer.garret@gmail.com>
2023-04-17 17:36:17 -0500

    Added six albums to the collection

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -8,4 +8,11 @@ function addToCollection( title, artist, yearPublished){ // adds a new album to 
     return newAlbum;
 } // end addToCollection
 
+addToCollection( "2001", "Dr. Dre", 1999 );
+addToCollection( "The Carter III", "Lil Wayne", 2008 );
+addToCollection( "The Carter V", "Lil Wayne", 2018 );
+addToCollection( "Illmatic", "Nas", 1994 );
+addToCollection( "Californication", "Red Hot Chili Peppers", 1999 );
+addToCollection( "Led Zeppelin IV", "Led Zepplin", 1971);
 
+console.log( collection );
